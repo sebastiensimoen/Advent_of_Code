8dfc20c32151d6caec6147dbc1847782964a7b81
sebastiensimoen <123328668+sebastiensimoen@users.noreply.github.com>
2023-01-23 20:13:50 +0100

    add files via upload

---
+++ 2022/day_01.py
@@ -0,0 +1,19 @@
+with open('file_01.txt') as file:
+    file.seek(0)
+    elf_calories = file.read()
+
+elf_calories_split = elf_calories.split("\n\n")
+array_calories = []
+
+for elf_calories in elf_calories_split:
+    elf_calories_lines = elf_calories.splitlines()
+    array_one_calorie = []
+    for food_item in elf_calories_lines:
+        array_one_calorie.append(int(food_item))
+    array_calories.append(sum(array_one_calorie))
+
+array_calories.sort(reverse = True)
+# Find the Elf carrying the most Calories.
+print(f"The Elf carrying the most calories carries {max(array_calories)} calories.")
+# Find the top three Elves carrying the most Calories.
+print(f"The top three Elves carrying the most calories carries {sum(array_calories[0:3])} calories.")
